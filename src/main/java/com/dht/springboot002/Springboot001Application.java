f615bbb52eaec4b68daab4b98bbc0d962b300923
zs <as@418577121.com>
2021-03-25 16:02:32 +0800

    我修改了

--- src/main/java/com/dht/springboot002/Springboot001Application.java
+++ src/main/java/com/dht/springboot002/Springboot001Application.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @MapperScan(basePackages = {"com.dht.springboot002.mapper"})
 @SpringBootApplication
 public class Springboot001Application {
-
+    //我新加了一行
     public static void main(String[] args) {
         SpringApplication.run(Springboot001Application.class, args);
     }
