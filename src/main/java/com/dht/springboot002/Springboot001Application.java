3d58b7a0ac40010c2815aefba03e5b2a9d2840dd
zs <as@418577121.com>
2021-03-25 16:29:46 +0800

    我修改了2

--- src/main/java/com/dht/springboot002/Springboot001Application.java
+++ src/main/java/com/dht/springboot002/Springboot001Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Springboot001Application {
     //我新加了一行
+    //test
     public static void main(String[] args) {
         SpringApplication.run(Springboot001Application.class, args);
     }
